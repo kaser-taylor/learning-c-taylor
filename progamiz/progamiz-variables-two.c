c63c4d192cb73a41d1f86507b4b18e7555962379
taylor <kaser.taylor@gmail.com>
2025-01-08 22:00:35 -0800

    new variables work

---
+++ progamiz/progamiz-variables-two.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int firstNumber = 33;
+    printf("firstNumber = %d", firstNumber);
+
+    int secondNumber = firstNumber;
+    printf("\nsecondNumber = %d" , secondNumber);
+
+    return 0;
+}
--- progamiz/programiz-variables.c
+++ progamiz/programiz-variables.c
@@ -4,11 +4,10 @@ int main() {
 
     int age =25;
     printf("Age: %d", age);
-    fflush(stdout);
+    
 
     age =31;
     printf("New age: %d", age);
-    fflush(stdout);
 
     return 0;
 }
