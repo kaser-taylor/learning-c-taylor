d2a0ca5ac5e965ebf557eb3e07aebcdaf6e6bc72
taylor <kaser.taylor@gmail.com>
2025-01-08 21:53:51 -0800

    learning how to assign variables

---
+++ progamiz/programiz-variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int age =25;
+    printf("Age: %d", age);
+    fflush(stdout);
+
+    age =31;
+    printf("New age: %d", age);
+    fflush(stdout);
+
+    return 0;
+}
